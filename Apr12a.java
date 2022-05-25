368cd5ad8b391cdfe610cd9f8c3141f6ba755331
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2022-05-25 19:57:44 -0400

    Add files via upload

---
+++ Apr12a.java
@@ -0,0 +1,12 @@
+package saunak.patel.ep2;
+
+public class Apr12a {
+    public static void main(String[] args) {
+        BPerson b = new BPerson("Sameer",19);
+        BTeacher a = new BTeacher("Rasamny", 30, true);
+
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}

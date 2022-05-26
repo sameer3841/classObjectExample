800b1839de9f935019e2fbb703d644bb90fb91f0
sameer3841 <81244405+sameer3841@users.noreply.github.com>
2022-05-26 11:05:37 -0400

    Update BPerson.java

--- BPerson.java
+++ BPerson.java
@@ -4,23 +4,29 @@ public class BPerson {
 
     public String name;
     private int age;
-
-    public BPerson(String Name, int Age){
-        name = Name;
-        age = Age;
+    
+    //indentifies the parameter values of an object
+    public BPerson(String name, int age){
+        this.name = name;
+        this.age = age;
     }
+    //prints name only
     public void printName(){
         System.out.println(this.name);
     }
+    //fetches the name only (alternative for print name, getter version)
     public String getName(){
         return name;
     }
+    //fetches the age only
     public int getAge(){
         return age;
     }
+    //sets a new name value
     public void setName(String name){
         this.name = name;
     }
+    //sets a new age value
     public void setAge(int age){
         this.age = age;
     }
